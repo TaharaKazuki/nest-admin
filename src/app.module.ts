b1aad1a474163e5a4c7ed75306d4006dde656405
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-21 20:28:15 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service'
+import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
